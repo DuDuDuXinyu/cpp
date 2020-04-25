e892e9f0f19bd034c839753eb5d0887c916f2b17
杜欣宇 <2331102566@qq.com>
2020-04-25 20:30:17 +0800

    类和对象的基本操作

--- ClassAndOBject.cpp
+++ ClassAndOBject.cpp
@@ -1,37 +1,15 @@
 #include <iostream>
+
+#pragma warning (disable:4996)
+
 using namespace std;
 
+#if 0
+//struct是用来描述学生信息并不是学生
 
-/*
-// C语言中的结构体中是不能放函数的
-// 面向过程：数据 和 操作数据的方法分离开的
 struct Student
 {
-	void InitStudent(struct Student* ps, char* name, char* gender, int age)
-	{}
-
-	char* name;
-	char* gender;
-	int age;
-};
-*/
-
-#if 0
-
-// C++中也有结构体---因为C++要兼容C语言
-// C++结构体中可以定义函数
-// 用Student结构体来描述学生信息
-// Student不是一个具体的学生
-
-// 用struct定义一个类--学生类
-// 用class定义一个类--学生类
-// 类：不是一个具体的实体，它是对该实体的描述
-// Student不是一个具体的学生，它是对群生群体的一种描述
-
-// 类的定义方式一：是将类的声明与定义全部放在类中进行定义
-class Student
-{
-	void InitStudent(char* name, char* gender, int age)
+	void InitStudent(const char* name, const char* gender, int age)
 	{
 		strcpy(_name, name);
 		strcpy(_gender, gender);
@@ -48,24 +26,11 @@ class Student
 	int _age;
 };
 
-
-/*
-两种定义方式的区别：
-1. 声明和定义分开---成员函数在类外定义时，必须要在成员函数名前加：类名::
-2. 如果声明和定义全部放在类中---编译器可能会将成员函数当成内联函数
-3. 一般情况下-都建议将声明和定义分开--为了防止重复包含
-*/
-
-
 int main()
 {
-	// Student不是具体的学生，它只不过是对学生的一种描述
-	// Student._age = 10;
+	//使用Student结构体定出一个结构体的学生变量
 
-	// 用Student结构体定义出来的结构体类型的变量--->称作为一个具体的学生
-	// 用Student定义出来的变量--->称为只对象，对象才算是一个真正的实体
-	Student s1,s2,s3;
-	/*
+	Student s1, s2, s3;
 	s1.InitStudent("Peter", "男", 30);
 	s2.InitStudent("静静", "女", 31);
 	s3.InitStudent("Summer", "公", 2);
@@ -73,28 +38,16 @@ int main()
 	s1.PrintStudent();
 	s2.PrintStudent();
 	s3.PrintStudent();
-	*/
-
-	// InitSutdent(&s1, "Peter", "男", 30);
-	// PrintStudent(&s1);
 	return 0;
 }
 #endif
 
 
 #if 0
-// 访问限定符
-// 面向对象：封装  继承  多态
-
-//  封装：将数据和操作数据的方法进行有机结合，隐藏对象的属性和实现细节，仅对外公开接口来和对象进行交互
-// C++中：通过类将数据和操作数据的方法包装起来
-// 使用访问权限：来控制那些方法可以再外部直接被进行调用
-// 访问限定符限定的是该成员是否能够在类外直接被使用，对类中访问没有限制
-
-class Student
+struct Student
 {
-public:// 想要在类外被直接调用，将该部分成员的访问权限设置为public
-	void InitStudent(char* name, char* gender, int age)
+public:
+	void InitStudent(const char* name, const char* gender, int age)
 	{
 		strcpy(_name, name);
 		strcpy(_gender, gender);
@@ -105,7 +58,6 @@ public:// 想要在类外被直接调用，将该部分成员的访问权限设置为public
 	{
 		cout << _name << "-" << _gender << "-" << _age << endl;
 	}
-
 private:
 	char _name[20];
 	char _gender[3];
@@ -113,9 +65,10 @@ protected:
 	int _age;
 };
 
-
 int main()
 {
+	//使用Student结构体定出一个结构体的学生变量
+
 	Student s1, s2, s3;
 	s1.InitStudent("Peter", "男", 30);
 	s2.InitStudent("静静", "女", 31);
@@ -125,53 +78,53 @@ int main()
 	s2.PrintStudent();
 	s3.PrintStudent();
 
-	// cout << s1._name << endl;
 	cout << s1._age << endl;
 	return 0;
 }
-
 #endif
 
 #if 0
-// 一个类就是一个作用域
-// C++作用域：全局作用域  函数体中的局部作用域  命名空间  类域
 
-void TestFunc()
+//c++作用域：全局作用域、函数体中作用域、命名空间、类域
+
+void TextFun()
 {}
 
-namespace N
+void fun()
 {
-	void TestFunc(int)
+	void TextFun()
 	{}
 }
 
-class Test
+namespace N
 {
-public:
-	void TestFunc(double)
+	void TextFun()
 	{}
+}
 
+class Text
+{
+	void TextFun()
+	{}
 };
 
-// TestFunc隶属于不同的作用域，因此不是函数重载
 int main()
 {
 	TestFunc();
-	N::TestFunc(10);
+	N::TestFunc();
 
 	Test t;
-	t.TestFunc(12.34);
+	t.TestFunc();
 	return 0;
 }
 #endif
 
 #if 0
-// 成员变量---相当于是类成员函数的全局变量---因为每个"成员函数"中都可以直接使用
-// 注意：和全局变量稍微有点区别，全局变量在使用前要先定义，而成员变量没有此要求，为什么？
+
 class Student
 {
-public:// 想要在类外被直接调用，将该部分成员的访问权限设置为public
-	void InitStudent(char* name, char* gender, int age)
+public:
+	void InitStudent(const char* name, const char* gender, int age)
 	{
 		strcpy(_name, name);
 		strcpy(_gender, gender);
@@ -182,7 +135,6 @@ public:// 想要在类外被直接调用，将该部分成员的访问权限设置为public
 	{
 		cout << _name << "-" << _gender << "-" << _age << endl;
 	}
-
 private:
 	char _name[20];
 	char _gender[3];
@@ -190,59 +142,11 @@ protected:
 	int _age;
 };
 
-// 在C语言中：一个变量在被使用之前必须先定义,否则就会编译失败
-
- int g_a = 10;
-
 int main()
 {
-	cout << g_a << endl;
-	return 0;
-}
+	//使用Student结构体定出一个结构体的学生变量
 
-// int g_a = 10;---如果放在使用之后定义，编译失败
-#endif
-
-#if 0
-class Student
-{
-public:// 想要在类外被直接调用，将该部分成员的访问权限设置为public
-	void InitStudent(char* name, char* gender, int age)
-	{
-		strcpy(_name, name);
-		strcpy(_gender, gender);
-		_age = age;
-	}
-
-	void PrintStudent()
-	{
-		cout << _name << "-" << _gender << "-" << _age << endl;
-	}
-
-	char _name[20];
-	char _gender[3];
-	int _age;
-};
-
-/*
-// 建造别墅：只知道我想要什么样子的别墅
-
-我：老板提需求--别墅                设计师：根据描述画出设计图纸画出图纸---程序员
-将别墅的描述高度给设计师，设计师根据描述画出设计图纸
-
-别墅的设计图纸(用来描述别墅)-----注意设计图纸还不能住人----->请匠人是否来建造----->才会形成一栋真真正正的别墅(可以住人的)
-
-   类(用来描述对象)--->不能直接使用其"成员"-----> 实例化---> 真真正正的对象（对象中才可以放数据）
-*/
-
-
-// 如何计算一个类的大小？
-// 必须要知道一个类中包含有什么？
-
-int main()
-{
 	Student s1, s2, s3;
-	
 	s1.InitStudent("Peter", "男", 30);
 	s2.InitStudent("静静", "女", 31);
 	s3.InitStudent("Summer", "公", 2);
@@ -252,36 +156,176 @@ int main()
 	s3.PrintStudent();
 
 	cout << sizeof(Student) << endl;
+
 	return 0;
 }
 #endif
 
-
+#if 0
 class A
 {
 public:
-	void funa()
-	{}
+	void fun();
 
 	int a;
 };
 
 class B
 {
-public:
-	void funb()
-	{}
+	void fun();
 };
 
 class C
-{};
+{
 
+};
 
 int main()
 {
-	cout << sizeof(A) << endl;
-	cout << sizeof(B) << endl;
-	cout << sizeof(C) << endl;
+	cout << sizeof(A) << endl;		//4
+	cout << sizeof(C) << endl;		//1
+	cout << sizeof(C) << endl;		//1
+}
+#endif
 
+#if 0
+class Student
+{
+public:
+	void InitStudent(const char* name, const char* gender, int age)
+	{
+		cout << _age << endl;
+		strcpy(this->_name, name);
+		strcpy(_gender, gender);
+		_age = age;
+	}
+
+	void PrintStudent()
+	{
+		cout << _name << "-" << _gender << "-" << _age << endl;
+		
+	}
+
+	void text(...)
+	{
+		cout << "不定参数的函数" << endl;
+	}
+private:
+	char _name[20];
+	char _gender[3];
+protected:
+	int _age=100;
+};
+
+int main()
+{
+
+	Student s1, s2, s3;
+	s1.InitStudent("Peter", "男", 30);
+	s2.InitStudent("静静", "女", 31);
+	s3.InitStudent("Summer", "公",10);
+
+	s1.PrintStudent();
+	s2.PrintStudent();
+	s3.PrintStudent();
+	s1.text(10);
+	s1.text();
+	s1.text(10,100);
+	s1.text(10,102,16);
+	return 0;
+}
+#endif
+
+#if 0
+
+class Student
+{
+public:
+	void InitStudent(const char* name, const char* gender, int age)
+	{
+		strcpy(_name, name);
+		strcpy(_gender, gender);
+		_age = age;
+	}
+
+	void PrintStudent()
+	{
+		cout << this << endl;
+		cout << _name << "-" << _gender << "-" << _age << endl;
+	}
+private:
+	char _name[20];
+	char _gender[3];
+protected:
+	int _age;
+};
+
+int main()
+{
+	//使用Student结构体定出一个结构体的学生变量
+
+	Student s1;
+	s1.InitStudent("Peter", "男", 30);
+
+	Student* ps = &s1;
+	ps->PrintStudent();
+
+
+	ps = nullptr;
+
+	//为空直接就无法访问，然后就会报错
+
+	ps->PrintStudent();
+
+	return 0;
+}
+#endif 
+
+class Time
+{
+public:
+	Time()
+	{
+
+	}
+	~Time()
+	{
+		cout << _year<<"xigouhanshu " << endl;
+	}
+	Time(int year)
+	{
+		_year = year;
+	}
+	Time(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void setTime(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void printTime()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year=1999;
+	int _month=1;
+	int _day=1;
+};
+
+int main()
+{
+	//哪一个先定义的哪一个最后一个执行析构函数
+	Time s;
+	Time t(2000);
+	
+	s.setTime(2020, 4, 5);
+	t.printTime();
+	s.printTime();
 	return 0;
 }
