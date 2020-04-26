704c8b22072b401726ff9d9f329571a6615c164c
杜欣宇 <2331102566@qq.com>
2020-04-26 21:53:54 +0800

    对象和类的基本操作（c++)的语法

--- ClassAndOBject.cpp
+++ ClassAndOBject.cpp
@@ -281,6 +281,7 @@ int main()
 }
 #endif 
 
+#if 0
 class Time
 {
 public:
@@ -328,4 +329,285 @@ int main()
 	t.printTime();
 	s.printTime();
 	return 0;
+}
+#endif
+#if 0 
+
+class String
+{
+public:
+	String(const char* str = nullptr)
+	{
+		_str = (char*)malloc(sizeof(char) * (1+strlen(str)));
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		if (_str)
+		{
+			free(_str);
+			_str = nullptr;
+		}
+		cout << "string析构函数" << endl;
+	}
+private:
+	char* _str;
+};
+void text()
+{
+	String s("hello");
+}
+int main()
+{
+	//在text中创建的String s，会在text函数退出时调用析构函数释放s的空间
+	text();
+	String s("hello123");
+	cout << 16513 << endl;
+	return 0;
+}
+#endif
+
+#if 0
+class Time
+{
+public:
+	~Time()
+	{
+		cout << _year << "xigou " << endl;
+	}
+	Time(int year = 1970, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	Time(const Time& t)
+	{
+		_year = t._year;
+		_month = t._month;
+		_day = t._day;
+		cout << "asfgasg" << endl;
+	}
+	void setTime(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void printTime()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Time d1(2020,4,6);
+	Time d3(1020, 4, 6);
+	Time d2(d1);
+	d2.printTime();
+	d1.printTime();
+
+	return 0;
+}
+#endif
+
+#if 0
+class String
+{
+public:
+	String(const char* str = nullptr)
+	{
+		_str = (char*)malloc(sizeof(char) * (1 + strlen(str)));
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		if (_str)
+		{
+			free(_str);
+			_str = nullptr;
+		}
+		cout << "string析构函数" << endl;
+	}
+private:
+	char* _str;
+};
+
+int main()
+{
+	String s1("hello");
+	String s2(s1);
+
+	return 0;
+}
+#endif
+#if 0
+enum Data
+{
+	ONE, TWO, THREE
+};
+int operator+(int left, Data right)
+{
+	return left + right;
+}
+
+int main()
+{
+
+}
+#endif
+
+#if 0
+class Time
+{
+public:
+	Time(int year = 1970, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	Time(const Time& t)
+	{
+		_year = t._year;
+		_month = t._month;
+		_day = t._day;
+		cout << "asfgasg" << endl;
+	}
+	void setTime(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void printTime()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+	//前置++
+	Time& operator++()
+	{
+		_day += 1;
+		return *this;
+	}
+	//后置++
+	Time operator++(int)
+	{
+		Time temp(*this);
+		_day += 1;
+		return temp;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Time d(2020, 4, 26);
+	Time r;
+	r = d++;
+	d.printTime();
+	r.printTime();
+	r = ++d;
+	d.printTime();
+	r.printTime();
+
+	return 0;
+}
+#endif
+#if 0
+
+class String
+{
+public:
+	String(const char* str = nullptr)
+	{
+		_str = (char*)malloc(sizeof(char) * (1 + strlen(str)));
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		if (_str)
+		{
+			free(_str);
+			_str = nullptr;
+		}
+		cout << "string析构函数" << endl;
+	}
+private:
+	char* _str;
+};
+
+int main()
+{
+	String s1("hello");
+	String s2("world");
+
+	s2 = s1;
+
+	return 0;
+}
+#endif 
+
+#if 0
+class Time
+{
+public:
+	Time(int year = 1970, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	Time(const Time& t)
+	{
+		_year = t._year;
+		_month = t._month;
+		_day = t._day;
+		cout << "asfgasg" << endl;
+	}
+	void printTime()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+	Time operator = (const Time& t)
+	{
+		if (this != &t)
+		{
+			_year = t._year;
+			_month = t._month;
+			_day = t._day;
+		}
+		return t;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Time d(2020, 4, 26);
+	Time r;
+	r.printTime();
+	r = d;
+	
+	r.printTime();
+	return 0;
+
+}
+#endif 
+
+
+int main()
+{
+
 }
